132dce01480bd62baa49baf15f8b4b2c691cd2fd
Alfa <mepuedeayudar@hotmail.com>
2019-03-22 11:19:20 -0300

    configuration parser complete

--- tp2/src/main/java/ar/edu/itba/ss/App.java
+++ tp2/src/main/java/ar/edu/itba/ss/App.java
@@ -4,6 +4,7 @@ package ar.edu.itba.ss;
 import ar.edu.itba.ss.MetricAlgoritms.OffLattice;
 import ar.edu.itba.ss.Model.Board;
 import ar.edu.itba.ss.Model.Particle;
+import ar.edu.itba.ss.utils.ConfigurationManager;
 import ar.edu.itba.ss.utils.DynamicFileGenerator;
 import ar.edu.itba.ss.utils.Metrics;
 
@@ -12,21 +13,28 @@ import java.util.List;
 public class App
 {
     public static void main( String[] args ) throws Exception {
-
+        ConfigurationManager c = new ConfigurationManager();
         DynamicFileGenerator dfg = new DynamicFileGenerator();
         Metrics m = new Metrics();
-        dfg.generateDymanicFile(15,50,10,0.01);
-        Board b = new Board(10,10,50,1,dfg.getParticles());
+
+        int numberParticles = c.getConfigurationParser().getNumberParticles();
+        double side = c.getConfigurationParser().getBoardSide();
+        double v = c.getConfigurationParser().getVelocity();
+        double etha = c.getConfigurationParser().getEtha();
+
+
+        dfg.generateDymanicFile((int) System.currentTimeMillis(),numberParticles,side, v);
+        Board b = new Board(side, (int) side,numberParticles,1,dfg.getParticles());
         OffLattice ol = new OffLattice();
         double time = 1;
         int n = 0;
         int printn = 1;
-        double printTime = 3;
-        while(n <= 10000) {
+        double printTime = c.getConfigurationParser().getPrint_time();
+        while(n <= c.getConfigurationParser().getTotal_time()) {
             m.addPolarization(b.getParticles());
             ol.calculateNeighbours(b);
-            List<Particle> particles = ol.calculateParticles(b,0.1,time);
-            b = new Board(10,10,50,1, particles);
+            List<Particle> particles = ol.calculateParticles(b,etha,time);
+            b = new Board(side, (int) side,numberParticles,1, particles);
             if(n*time % printTime == 0) {
                 dfg.saveDynamicFile(b,printn);
                 printn++;
--- tp2/src/main/java/ar/edu/itba/ss/utils/DynamicFileGenerator.java
+++ tp2/src/main/java/ar/edu/itba/ss/utils/DynamicFileGenerator.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class DynamicFileGenerator {
+public class    DynamicFileGenerator {
 
     private List<Particle> particles;
 
